778685002a19fd3d70f574e3091eadf2480425dc
Michael Wang <mkwguru@gmail.com>
2013-10-12 16:13:46 -0700

    Added date picker, also added phone number field. Automatically pull device phone number and populate the phone number field.

--- FoodDonorAndroid/src/org/wafoodcoalition/givecamp/fooddonor/Donate.java
+++ FoodDonorAndroid/src/org/wafoodcoalition/givecamp/fooddonor/Donate.java
@@ -1,15 +1,27 @@
 package org.wafoodcoalition.givecamp.fooddonor;
 
+import java.util.Calendar;
+
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Context;
+import android.telephony.TelephonyManager;
 import android.view.Menu;
+import android.widget.DatePicker;
+import android.widget.EditText;
 
 public class Donate extends Activity {
-
+	
+	private EditText phone;
+	private DatePicker dpResult;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_donate);
+		
+		setDefaultPhoneOnView();
+		setCurrentDateOnView();
 	}
 
 	@Override
@@ -18,5 +30,30 @@ public class Donate extends Activity {
 		getMenuInflater().inflate(R.menu.activity_donate, menu);
 		return true;
 	}
-
+	
+	// display current date
+	public void setCurrentDateOnView() {
+	 
+		dpResult = (DatePicker) findViewById(R.id.dpResult);
+	 
+		final Calendar c = Calendar.getInstance();
+		int year = c.get(Calendar.YEAR);
+		int month = c.get(Calendar.MONTH);
+		int day = c.get(Calendar.DAY_OF_MONTH);
+	 
+		// set current date into date picker
+		dpResult.init(year, month, day, null);
+	}
+	
+	public void setDefaultPhoneOnView() {
+		
+		phone = (EditText) findViewById(R.id.phone);
+		
+		TelephonyManager tMgr =(TelephonyManager)getSystemService(Context.TELEPHONY_SERVICE);
+		String phoneNumber = tMgr.getLine1Number();
+		
+		if (phoneNumber != null) {
+			phone.setText(phoneNumber);
+		}
+	}
 }
