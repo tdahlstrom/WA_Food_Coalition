e48b07de90b8f059c8918e25001cf20e81e730ea
Abe Diaz (@abe238) <info@abediaz.com>
2013-10-12 11:36:29 -0700

    Donation Model

---
+++ API/API/Controllers/DonationController.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using API.Models;
+
+namespace API.Controllers
+{
+    public class DonationController : ApiController
+    {
+        private DonationDBContext db = new DonationDBContext();
+
+        // GET api/Donation
+        public IQueryable<Donation> GetDonations()
+        {
+            return db.Donations;
+        }
+
+        // GET api/Donation/5
+        [ResponseType(typeof(Donation))]
+        public IHttpActionResult GetDonation(int id)
+        {
+            Donation donation = db.Donations.Find(id);
+            if (donation == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(donation);
+        }
+
+        // PUT api/Donation/5
+        public IHttpActionResult PutDonation(int id, Donation donation)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != donation.ID)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(donation).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!DonationExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return Ok();
+        }
+
+        // POST api/Donation
+        [ResponseType(typeof(Donation))]
+        public IHttpActionResult PostDonation(Donation donation)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Donations.Add(donation);
+            db.SaveChanges();
+
+            return CreatedAtRoute("DefaultApi", new { id = donation.ID }, donation);
+        }
+
+        // DELETE api/Donation/5
+        [ResponseType(typeof(Donation))]
+        public IHttpActionResult DeleteDonation(int id)
+        {
+            Donation donation = db.Donations.Find(id);
+            if (donation == null)
+            {
+                return NotFound();
+            }
+
+            db.Donations.Remove(donation);
+            db.SaveChanges();
+
+            return Ok(donation);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+
+        private bool DonationExists(int id)
+        {
+            return db.Donations.Count(e => e.ID == id) > 0;
+        }
+    }
+}
--- API/API/Models/Donation.cs
+++ API/API/Models/Donation.cs
@@ -1,11 +1,48 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
+using System.Data.Entity;
+using System.Data.Spatial;
 
 namespace API.Models
 {
     public class Donation
     {
+        [Required]
+        [Key]
+        [Display(Name = "ID")]
+        public int ID { get; set; }
+
+        [Required]
+        [Display(Name = "Name")]
+        public string Name { get; set; }
+
+        [Required]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+
+        [Required]
+        [Display(Name = "Amount of Food")]
+        public string FoodAmount { get; set; }
+
+        [Required]
+        [Display(Name = "Type of Food")]
+        public string FoodType { get; set; }
+
+        //[Required]
+        //[Display(Name = "Location")]
+        //public DbGeography Location { get; set; }
+    }
+
+    public class DonationDBContext : DbContext
+    {
+        public DonationDBContext()
+            : base("DefaultConnection")
+        {
+
+        }
+        public DbSet<Donation> Donations { get; set; }
     }
 }
