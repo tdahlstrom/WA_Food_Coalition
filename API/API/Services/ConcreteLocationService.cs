b3b4beb89e4965e1a227073f381bfbb02d5459af
esieg <erics@slalom.com>
2013-10-12 17:01:58 -0700

    Adding FoodBankController and LocationService
    These classes all still need to be implemented, just getting them into
    source control

---
+++ API/API/Services/ConcreteLocationService.cs
@@ -0,0 +1,19 @@
+﻿using API.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace API.Services {
+    public class ConcreteLocationService : ILocationService {
+
+        public List<FoodBank> GetNearbyFoodBanks(double latitude, double longitude, int amountToReturn) {
+            throw new NotImplementedException();
+        }
+
+
+        public List<Donation> GetNearbyDonations(FoodBank foodBank) {
+            throw new NotImplementedException();
+        }
+    }
+}
